cf9777623abc44f21e26978c85721f0c66c83a0c
Benjamin Winchester <benjamin.m.winchester@googlemail.com>
2021-07-28 10:38:51 -0400

    Add @babel/preset-env and an example test

--- src/App.test.js
+++ src/App.test.js
@@ -6,10 +6,10 @@ import "@testing-library/jest-dom/extend-expect";
 
 import { render } from "@testing-library/svelte";
 
-import Comp from "./App.svelte";
+import AppComponent from "./App.svelte";
 
 test("shows proper heading when rendered", () => {
-  const { getByText } = render(Comp, { name: "World" });
+  const { getByText } = render(AppComponent, { name: "World" });
 
   expect(getByText("Hello World!")).toBeInTheDocument();
 });
